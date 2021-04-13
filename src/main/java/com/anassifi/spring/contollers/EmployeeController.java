52f3cea0fb6311f8ae28b22ec4ad65af09f7d81c
Anassifi <abdelhakimnassifi@gmail.com>
2021-04-13 19:00:32 +0000

    First controller
    @RestController indicates that the data returned by each method will be written straight into the response body instead of rendering a template.
    
    We have routes for each operation (@GetMapping, @PostMapping, @PutMapping and @DeleteMapping, corresponding to HTTP GET, POST, PUT, and DELETE calls).

--- src/main/java/com/anassifi/spring/contollers/EmployeeController.java
+++ src/main/java/com/anassifi/spring/contollers/EmployeeController.java
@@ -6,9 +6,11 @@ import java.util.Optional;
 import com.anassifi.spring.entities.Employee;
 import com.anassifi.spring.repositories.EmployeeRepository;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -22,26 +24,35 @@ public class EmployeeController {
     };
 
     @GetMapping("/employees")
-    List<Employee> getEmployees() {
+    public List<Employee> getEmployees() {
         return employeeRepository.findAll();
     }
 
     @GetMapping("/employee/{id}")
-    Optional<Employee> getEmployee(@PathVariable Long id) {
+    public Optional<Employee> getEmployee(@PathVariable Long id) {
         return employeeRepository.findById(id);
     }
 
     @PostMapping("/employees")
-    Employee saveEmployee(@RequestBody Employee newEmployee) {
+    public Employee saveEmployee(@RequestBody Employee newEmployee) {
         return employeeRepository.save(newEmployee);
     }
 
-    public EmployeeRepository getEmployeeRepository() {
-        return this.employeeRepository;
+    @PutMapping("/employee/{id}")
+    public Employee replaceEmployee(@RequestBody Employee newEmployee, @PathVariable Long id) {
+        return employeeRepository.findById(id).map(newEmployee -> {
+            newEmployee.setName(newEmployee.getName());
+            newEmployee.setRole(newEmployee.getRole());
+            return employeeRepository.save(newEmployee);
+        }).orElseGet(() -> {
+            newEmployee.setId(id);
+            return employeeRepository.save(newEmployee);
+        });
     }
 
-    public void setEmployeeRepository(EmployeeRepository employeeRepository) {
-        this.employeeRepository = employeeRepository;
+    @DeleteMapping("/employee/{id}")
+    public Employee removeEmployee(@PathVariable Long id){
+        employeeRepository.deleteById(id);
     }
 
 }
